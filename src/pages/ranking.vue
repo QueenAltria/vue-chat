4762fb3fd40d416086f04f401f37b2d80ad6052b
JP <471040339@qq.com>
2019-04-07 18:09:28 +0800

    数据更改

--- src/pages/ranking.vue
+++ src/pages/ranking.vue
@@ -1,6 +1,19 @@
 <template>
 	<div>
-		<h1>排行</h1>
+    <div class="rank-top">
+      <div class="rank-top-item">
+        日排行
+      </div>
+
+      <div class="rank-top-item">
+        月排行
+      </div>
+
+      <div class="rank-top-item">
+        总排行
+      </div>
+
+    </div>
 	</div>
 </template>
 
@@ -26,4 +39,19 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.rank-top{
+  display: flex;
+  margin-top: 10px;
+  font-size: 0.8em;
+}
+
+.rank-top-item{
+  flex: 1;
+  text-align: center;
+  border: 1px solid #32b66b;
+  padding: 5px;
+  margin: 0 10px;
+  border-radius: 4px;
+}
+
 </style>
